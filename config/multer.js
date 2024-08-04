5af43ceda975f0ec2ff3ad2f0c7976c17b0a7f72
Muxitdin <toraananas@gmail.com>
2024-08-04 18:55:33 +0500

    covered the route handler with trycatch block

--- config/multer.js
+++ config/multer.js
@@ -17,7 +17,6 @@ dotenv.config();
 // Create storage engine
 const storage = new GridFsStorage({
     url: process.env.MONGO_URI,
-    options: { useNewUrlParser: true, useUnifiedTopology: true },
     file: (req, file) => {
         console.log("file info: " + file)
         return {
--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -4,37 +4,18 @@ import upload from "../config/multer.js";
 const router = express.Router();
 
 router.post("/", upload.single("file"), (req, res) => {
-    console.log(req.file);
-    if (!req.file) {
-        return res.status(400).json({ message: 'File upload failed' });
+    try {
+        console.log(req.file);
+        if (!req.file) {
+            return res.status(400).json({ message: 'File upload failed' });
+        }
+
+        res.json({
+            imgUrl: `https://promptopia-back.onrender.com/${req.file.filename}`,
+        }).status(200);
+    } catch (error) {
+        console.log(error);
     }
-
-    const baseUrl = 'https://promptopia-back.onrender.com'
-        
-    res.json({
-        imgUrl: `${baseUrl}/${req.file.filename}`,
-    }).status(200);
 })
-
-// router.get("/files/:filename", async (req, res) => {
-//     const conn = mongoose.connection;
-//     const bucket = new GridFSBucket(conn.db, {
-//         bucketName: 'uploads',
-//     });
-
-//     const downloadStream = bucket.openDownloadStreamByName(req.params.filename);
-
-//     downloadStream.on('data', (chunk) => {
-//         res.write(chunk);
-//     });
-
-//     downloadStream.on('error', () => {
-//         res.sendStatus(404);
-//     });
-
-//     downloadStream.on('end', () => {
-//         res.end();
-//     });
-// });
 
 export default router;
