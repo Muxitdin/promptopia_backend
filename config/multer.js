aa9cc4193010108a0b93390c196f2a32040870b0
Muxitdin <toraananas@gmail.com>
2024-08-05 11:37:46 +0500

    some changes

--- config/multer.js
+++ config/multer.js
@@ -1,32 +1,32 @@
 import multer from "multer";
-// import { GridFsStorage } from 'multer-gridfs-storage'
+import { GridFsStorage } from 'multer-gridfs-storage'
 import dotenv from "dotenv";
 dotenv.config();
 
-// Multer configuration
-const storage = multer.diskStorage({
-    // destination: function (req, file, cb) {
-    //     cb(null, 'uploads/')
-    // }
-    destination: "./uploads",
-    filename: (_, file, cb) => {
-        cb(null, file.originalname)
-    }
-})
+// // Multer configuration
+// const storage = multer.diskStorage({
+//     // destination: function (req, file, cb) {
+//     //     cb(null, 'uploads/')
+//     // }
+//     destination: "./uploads",
+//     filename: (_, file, cb) => {
+//         cb(null, file.originalname)
+//     }
+// })
 
-// // Create storage engine
-// const storage = new GridFsStorage({
-//     url: process.env.MONGO_URI,
-//     file: (req, file) => {
-//         console.log("file info: " + file)
-//         return {
-//             bucketName: 'uploads', // The bucket name in MongoDB
-//             filename: `${Date.now()}-${file.originalname}`,
-//         };
-//     },
-// });
-// const upload = multer({ storage });
+// Create storage engine
+const storage = new GridFsStorage({
+    url: process.env.MONGO_URI,
+    file: (req, file) => {
+        console.log("file info: " + file)
+        return {
+            bucketName: 'uploads', // The bucket name in MongoDB
+            filename: `${file.originalname}`,
+        };
+    },
+});
+const upload = multer({ storage });
 
-const upload = new multer({ storage: storage });
+// const upload = new multer({ storage: storage });
 
 export default upload;
